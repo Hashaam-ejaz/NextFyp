876e0b60fc01e46f43773e1fd36fa27e8e6c30bd
HashaamBinEjaz <139843321+HashaamBinEjaz@users.noreply.github.com>
2024-03-19 13:22:05 +0500

    initial project setup + noor signin page temp

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,14 @@
+"Use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button className="bg-red" onClick={() => console.log("Yopu Clickde")}>
+        click me
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddToCart from "./AddToCart";
+
+const ProductCard = () => {
+  return (
+    <div className="bg-red-900">
+      <AddToCart />
+    </div>
+  );
+};
+
+export default ProductCard;
