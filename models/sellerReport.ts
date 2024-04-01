51c33472e2258cf01cfa62854365868284dd24c6
Rai Waqas <raiwaqas515@gmail.com>
2024-04-01 19:53:22 +0500

    Added SellerReport in the database

---
+++ models/sellerReport.ts
@@ -0,0 +1,42 @@
+import { Schema, model, Types } from "mongoose";
+
+interface ISellerReport {
+  userID: Types.ObjectId;
+  rating: number;
+  noOfProductsSold: number;
+  totalSalesAmount: number;
+  balance: number;
+  withdrawals: number;
+}
+
+const SellerReportSchema = new Schema<ISellerReport>({
+  userID: {
+    type: Schema.Types.ObjectId, ref: 'User',
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  noOfProductsSold: {
+    type: Number,
+    required: true,
+  },
+  totalSalesAmount: {
+    type: Number,
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+  withdrawals: {
+    type: Number,
+    required: true,
+  },
+});
+
+const SellerReport = model<ISellerReport>("SellerReport", SellerReportSchema);
+
+export { SellerReport };
+export type { ISellerReport };
