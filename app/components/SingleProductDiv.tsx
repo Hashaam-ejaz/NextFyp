b40eb3bc05ca0d41a4671910ac02e761219494b0
HashaamBinEjaz <139843321+HashaamBinEjaz@users.noreply.github.com>
2024-05-18 11:46:14 +0500

    Single Product Tile Div added.

---
+++ app/components/SingleProductDiv.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { IProduct } from "@/models/products";
+import Image from "next/image";
+import Link from "next/link";
+
+const SingleprodDiv: React.FC<{ prod: IProduct }> = ({ prod }) => {
+  const product = prod?.id;
+
+  const StarRating = ({ rating }: { rating: number }) => {
+    const stars: JSX.Element[] = [];
+
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(
+          <span key={i} className="text-[#FA8232]">
+            &#9733;
+          </span>
+        );
+      } else {
+        stars.push(
+          <span key={i} className="text-[#77878F]">
+            &#9734;
+          </span>
+        );
+      }
+    }
+    return <div>{stars}</div>;
+  };
+
+  return (
+    // Check if prod and its id exist
+    <Link href={`/products/${prod._id}`}>
+      <div
+        id="singleprodDiv"
+        className="flex flex-col self-center w-[14.625rem] h-[20rem] transition-shadow shadow hover:shadow-xl ease-in-out duration-300 rounded-[0.5px]"
+      >
+        <div
+          id="prodImage"
+          className="relative w-[12.625rem] h-[11rem] m-[1rem] self-center"
+        >
+          <Image src={prod?.images[0].src} alt="prod" className="h-auto" fill />
+        </div>
+        <div
+          id="prodDetails"
+          className=" flex flex-row px-2 mx-[1rem] mb-[1rem] mt-[0.5rem] justify-center"
+        >
+          <div className="flex flex-col h-full w-full ">
+            <div id="rating" className="flex flex-row text-[5.61%] md:text-xs ">
+              <StarRating rating={prod?.avgRating} />
+              <span className="text-[#77878F]">
+                {"("}
+                {prod?.noReviews}
+                {")"}
+              </span>
+            </div>
+            <div
+              id="prodName"
+              className="flex flex-row text-[#191C1F] text-xs md:text-sm font-normal mt-1  "
+            >
+              <p className="text-ellipsis inline-block">{prod?.name}</p>
+            </div>
+            <div
+              id="prodPrice"
+              className="flex flex-row text-[#954BAF] text-sm mt-1 font-semibold"
+            >
+              PKR{"   "}
+              {prod?.price}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default SingleprodDiv;
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,42 @@
 "use client";
 import { signOut } from "next-auth/react";
+import SingleProductDiv from "../components/SingleProductDiv";
+import { useEffect, useState } from "react";
+import { IProduct } from "@/models/products";
 
 const Page = () => {
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/products/");
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <h1>This is the home Page</h1>
-      <div className="bg-red-400 w-fit rounded-lg px-4 py-2 cursor-pointer">
+      <div className="bg-red-400 w-fit rounded-lg px-4 py-2 cursor-pointer m-16">
         <button onClick={() => signOut({ callbackUrl: "/login" })}>
           Logout
         </button>
       </div>
+      {products.map((product, index) => (
+        <div
+          key={product.id || index}
+          className="snap-start inline-block  cursor-pointer"
+        >
+          <SingleProductDiv prod={product} />
+        </div>
+      ))}
     </div>
   );
 };
